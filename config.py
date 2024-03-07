be6e049ce09790f09948906f57d63723faf3830d
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2024-03-07 20:36:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Programmerofbots/Music_Bot",
+    "https://github.com/Samdaveloper/HACKER-X-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/O_P_Hacker")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Princes_log_support_group")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/The_Caption_XD")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/THE_MUSIC_CLUB_0143")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/52792e7acc085c69eeb14.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/b1fdec5203c41b4ae5570.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/52792e7acc085c69eeb14.jpg"
